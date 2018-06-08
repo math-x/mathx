1618393be285fb738abdfb98daf9a81f73edc550
kanav99 <kanav0610@gmail.com>
2018-06-08 11:25:27 +0530

    Adds basic calculus

---
+++ lib/calculus.h
@@ -0,0 +1,42 @@
+/*
+ * Calculus capabilities for MathX
+ * Assumes that a mathematical continous function is of form `long double func (long double)`
+ */
+#ifndef CALCULUS_MATHX_
+
+#define CALCULUS_MATHX_
+#define dx 0.00001
+#define NEWTON_RAPHSON_PRECISION 10
+#define INF 1e12
+// Function which returns the derivative of mathematical function `fn` at `x`
+long double differentiate(long double (*fn)(long double), long double x) {
+	long double rhd = (fn(x + dx) - fn(x)) / dx;
+	long double lhd = (fn(x) - fn(x - dx)) / dx;
+	if(fabs(rhd - lhd) < (dx))
+		return (rhd + lhd)/2;
+	else {
+		std::cerr << "Then given function is not differentiable at " << x << '\n';
+	}
+}
+// Function which returns the integral of mathematical function `fn` from `ll` to `ul`
+long double integrate(long double (*fn)(long double), long double ll, long double ul) {
+	if(ul < ll)
+		return -integrate(fn, ul, ll);
+	long double result = 0;
+	while(ul >= ll) {
+		result += (fn(ll) * dx);
+		ll += dx;
+	}
+	return result;
+}
+/* Function which returns the root with the help of an initial `guess` using Newton-Raphson Method
+ * Generally returns the nearest root to `guess`
+ */
+long double apx_root (long double (*fn)(long double), long double guess) {
+	for (int i = 0; i < NEWTON_RAPHSON_PRECISION; ++i)
+	{
+		guess = guess - (fn(guess) / differentiate(fn, guess));
+	}
+	return guess;
+}
+#endif
--- mathx.h
+++ mathx.h
@@ -1,4 +1,4 @@
 #include "lib/number_theory.h"
 #include "lib/arithmetic.h"
-#include "lib/trigonometry.h"
 #include "lib/algebra.h"
+#include "lib/calculus.h"
