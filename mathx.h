ec8219a7382ef839b8aa9015cc59b6067db324af
kanav99 <kanav0610@gmail.com>
2018-07-03 12:53:06 +0530

    Removes trignometry.h

--- mathx.h
+++ mathx.h
@@ -1,7 +1,6 @@
 #include <cmath>
 #include "lib/number_theory.h"
 #include "lib/arithmetic.h"
-#include "lib/trigonometry.h"
 #include "lib/algebra.h"
 #include "lib/data_structures.h"
 #include "lib/int_l.h"
